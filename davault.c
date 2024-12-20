1d33340bc525b2a9fb1e812c5c1f9f0cd2f2c254
Max D <maximdedesbans@gmail.com>
2024-12-20 22:03:08 +0100

    Added the creation of a file for the passwords to be stored.

--- davault.c
+++ davault.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
+void create_vault(char* vault_name);
+
 int main(int argc, char *argv[]) {
 
 	printf("Welcome to DaVault\n");
@@ -9,6 +11,33 @@ int main(int argc, char *argv[]) {
 	// Print list of possible actions 
 	printf("1) create a new vault\n");
 
+	// Prompt user input for choice
+	int choice;
+	scanf("Input: %d \n", &choice);
+
+	if (choice == 1) {
+		// ask user for file name
+		char vault_name[30];
+		scanf("Vault name: %s\n", &vault_name);
+		// create new file
+		create_vault(vault_name	);
+
+		printf("Please provide a password\n");
+
+
+		// ask for password
+		// verify password
+		// add password to file
+	}
+
 	return 0;
 
+}
+
+void create_vault(char* vault_name) {
+		FILE* fptr;
+
+		fopen(vault_name, "w");
+
+		fclose(fptr);
 }
