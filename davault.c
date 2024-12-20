4b41b4b678437a1936c09f32a114361220a1adc8
Max D <maximdedesbans@gmail.com>
2024-12-20 16:28:36 +0100

    First commit. C program with greetings

---
+++ davault.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+	printf("Welcome to DaVault\n");
+	printf("What would you like to do?\n");
+	
+	// Print list of possible actions 
+	printf("1) create a new vault\n");
+
+	return 0;
+
+}
