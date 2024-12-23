197e46f7a75704fab7fb645f522776a3727bcd06
Max D <maximdedesbans@gmail.com>
2024-12-23 22:34:00 +0100

    Add option to create a vault. Addes encryption to file used as vault.

--- davault.c
+++ davault.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 void create_vault(char* vault_name);
@@ -9,35 +10,52 @@ int main(int argc, char *argv[]) {
 	printf("What would you like to do?\n");
 	
 	// Print list of possible actions 
-	printf("1) create a new vault\n");
+	printf("1) Create a new vault\n");
+	printf("2) Sign in to vault\n");
+	printf("3) Add a password\n");
+	printf("4) Get a password\n");
+	printf("Press q to quit.\n");
 
 	// Prompt user input for choice
 	int choice;
 	scanf("Input: %d \n", &choice);
 
+	// Create a new vault.
 	if (choice == 1) {
 		// ask user for file name
 		char vault_name[30];
 		scanf("Vault name: %s\n", &vault_name);
 		// create new file
-		create_vault(vault_name	);
+		create_vault(vault_name);
+
+		// Create gpg command for system() function
+		char command[50];
+		sprintf(command, "gpg -c %s", vault_name);
 
 		printf("Please provide a password\n");
+		system(command);
 
-
-		// ask for password
-		// verify password
-		// add password to file
+		// remove text file
+		char rm_file[35];
+		sprintf(rm_file,"rm %s", vault_name);
+		system(rm_file);
+	} else if (choice == 2) {
+		// Get vault name
+		// Look for vault
+		// If vault exists, login
+		// else error
 	}
+
 
 	return 0;
 
 }
 
 void create_vault(char* vault_name) {
-		FILE* fptr;
+	// create a file with vault_name
+	FILE* fptr;
 
-		fopen(vault_name, "w");
+	fopen(vault_name, "w");
 
-		fclose(fptr);
+	fclose(fptr);
 }
