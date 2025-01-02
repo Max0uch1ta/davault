d43be3d9c4ed706529e72641f6e1d7dd89629651
Max D <maximedesbans@gmail.com>
2025-01-02 14:28:17 +0100

    Add possiibility to save new passwords.
    Refactored so you could save new pwd, and reencrypt file.

--- davault.c
+++ davault.c
@@ -12,15 +12,17 @@
 
 void create_vault(char* vault_name);
 char * get_vaults(char* path, char* file, bool encrypted);
-void open_vault(char* vault_name);
+char* open_vault();
 void get_creds(char* line);
+void rm_eol(char txt[]);
+void encrypt_file(char* vault_name, char* filename); 
 
 
 int main(int argc, char *argv[]) {
 	// Check that necessary directories are present
 	system("[ ! -d \"~/.local/share/davault/.vaults/\" ] && mkdir -p ~/.local/share/davault/.vaults/");
-	FILE* current_vault;
-	char* vault_name;
+	FILE* current_vault = NULL;
+	char* vault_name = NULL;
 	bool vault_open = false;
 
 	printf("Welcome to DaVault\n");
@@ -50,30 +52,37 @@ int main(int argc, char *argv[]) {
 				scanf("%ms", &vault_name);
 				// create new vault
 				create_vault(vault_name);
+				vault_name = NULL;
 				break;
 			// Open a vault
 			case 2:
-				open_vault(vault_name);
+				vault_name = open_vault();
+				vault_open = true;
 				break;
 
 			case 3:
 				// if no vault open, ask what vault to open
-				if (vault_name == NULL) {
-					open_vault(vault_name);
+				if (!vault_open) {
+					vault_name = open_vault();
+					vault_open = true;
+					
 				}
 				// open file 
 				current_vault = fopen("vault", "a");
 				if (current_vault == NULL) {
 					printf("Could not open file.\n");
 				}
-				// Get credentials dor stdin
+				// Get credentials for stdin
 				char line[100];
+				while (getchar() != '\n');
 				get_creds(line);
 				
 				// write credential to file.
 				if (fprintf(current_vault, "%s", line) < 0) { printf("Error writing to file");} 
 
 				fclose(current_vault); // close the file
+				encrypt_file(vault_name, "vault");
+
 				break;
 			case 4:
 				// if no vault open, ask what vault
@@ -97,11 +106,15 @@ void create_vault(char* vault_name) {
 	fprintf(fptr, "site, username, password\n");
 
 	fclose(fptr);
+	encrypt_file(vault_name, vault_name);
 
+}
+
+void encrypt_file(char* vault_name, char* filename) {
 
 	// Create gpg command for system() function
 	char command[MAX_CMD_LEN];
-	sprintf(command, "gpg -o ~/.local/share/davault/.vaults/%s.gpg -c %s", vault_name, vault_name);
+	sprintf(command, "gpg -o ~/.local/share/davault/.vaults/%s.gpg -c %s", vault_name, filename);
 
 	printf("Please provide a password\n");
 	system(command);
@@ -120,32 +133,32 @@ char * get_vaults(char* path, char* file, bool encrypted) {
 	// if looking for encrypted vault add .gpg to path
 	char *fullpath;
 	if (encrypted) {
-		fullpath = malloc(snprintf(NULL, 0, "%s%s%s.gpg", home, path, file));
+		fullpath = malloc((snprintf(NULL, 0, "%s%s%s.gpg", home, path, file)+1) * sizeof(char)); // sizeof(char) redundant because = 1
 		sprintf(fullpath, "%s%s%s.gpg", home, path, file);
 
 	} else {
-		fullpath = malloc(snprintf(NULL, 0, "%s%s%s", home, path, file));
+		fullpath = malloc((snprintf(NULL, 0, "%s%s%s", home, path, file)+1) * sizeof(char));
 		sprintf(fullpath, "%s%s%s", home, path, file);
 	}
 
 	return fullpath;
 }
 
-void open_vault(char* vault_name) {
-	bool closed = true;
-	while (closed) {
-		char *tmp;
+char* open_vault() {
+	bool vault_open = false;
+	char *vault_name;
+	while (!vault_open) {
 		// Get vault name
 		printf("What vault do you want to open? (0 to return)\n> ");
-		if (scanf("%ms", &tmp) != 1) {
+		if (scanf("%ms", &vault_name) != 1) {
 			printf("An error occured");
 		}
 		// Exit if 0
-		if (strcmp(tmp, "0") == 0) {
+		if (strcmp(vault_name, "0") == 0) {
 			break;
 		}
 		// Test if vault exists
-		char* path = get_vaults(VAULT_PATH, tmp, true);
+		char* path = get_vaults(VAULT_PATH, vault_name, true);
 
 		if(!access(path, F_OK )){
 			printf("The File %s\t was Found\n",path);
@@ -153,14 +166,13 @@ void open_vault(char* vault_name) {
 			char cmd[MAX_CMD_LEN];
 			sprintf(cmd,"gpg -o vault --decrypt %s", path);
 			system(cmd);
-			strcpy(vault_name, tmp);
-			closed = true;
+			vault_open = true;
 		}else{
 			printf("The File %s\t not Found\n",path);
 		}
-		free(tmp);
 		free(path);
 	}
+	return vault_name;
 }
 
 
@@ -172,8 +184,22 @@ void get_creds(char* line) {
 	char password[50];
 	printf("What is the source of the pwd\n>");
 	if (fgets(source, 25, stdin) == NULL) { printf("Error getting source");}
+	rm_eol(source);
+	printf("What is the username of the pwd\n>");
 	if (fgets(username, 25, stdin) == NULL) { printf("Error getting source");}
-	if (fgets(password, 55, stdin) == NULL) { printf("Error getting source");}
+	rm_eol(username);
+	printf("What is the password of the pwd\n>");
+	if (fgets(password, 50, stdin) == NULL) { printf("Error getting source");}
+	rm_eol(password);
 
-	snprintf(line, 100, "%s;%s;%s", source,username,password);
+	snprintf(line, 100, "%s,%s,%s", source,username,password);
+}
+
+// Remove end of line from end of string
+void rm_eol(char txt[]) {
+	for (int i=0; i<strlen(txt); i++) {
+		if(txt[i] == '\n') {
+			txt[i] = '\0';
+		}
+	}
 }
